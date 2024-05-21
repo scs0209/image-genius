95df4db5115fccdf7897d7787b660af07a030ec8
scs0209 <tjdckdtn2463@naver.com>
2024-05-21 16:13:17 +0900

    feat: MobileNaviagtion바 퍼블리싱 완성

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,3 +1,4 @@
+import MobileNav from "@/components/shared/MobileNav";
 import SideBar from "@/components/shared/SideBar";
 import React from "react";
 
@@ -5,6 +6,8 @@ const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
       <SideBar />
+      <MobileNav />
+
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>

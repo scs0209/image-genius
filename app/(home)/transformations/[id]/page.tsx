ffc2747ba7f2a09c45a8778acc31197cb16de644
scs0209 <tjdckdtn2463@naver.com>
2024-05-21 14:12:35 +0900

    chore: 라우팅 경로 추가

---
+++ app/(home)/transformations/[id]/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const TransformationPage = () => {
+  return <div>TransformationPage</div>;
+};
+
+export default TransformationPage;
+
+// 그룹 라우팅 적용
+// http://localhost:3000/transfromations/[id]

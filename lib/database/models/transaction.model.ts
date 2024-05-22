7d3cbb816d763e1a9e5d1d87946df23699c5390c
scs0209 <tjdckdtn2463@naver.com>
2024-05-22 23:55:42 +0900

    feat: mongodb 모델 셋팅 완성

---
+++ lib/database/models/transaction.model.ts
@@ -0,0 +1,46 @@
+import { Document, Schema, model, models } from "mongoose";
+
+export interface ITransaction extends Document {
+  _id: string;
+  createdAt: Date;
+  stripeId: string;
+  amount: number;
+  plan?: string;
+  credits?: number;
+  buyer: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+  };
+}
+
+const TransactionSchema = new Schema({
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  stripeId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  plan: {
+    type: String,
+  },
+  credits: {
+    type: Number,
+  },
+  buyer: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+const Transaction =
+  models?.Transaction || model("Transaction", TransactionSchema);
+
+export default Transaction;
---
+++ lib/database/mongoose.ts
@@ -0,0 +1,36 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+interface MongooseConnection {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+// optimize 하기 위해 캐싱을 하려고 함 아니면 작업을 할 때마다 mongodb에 연결을 하게 되서 불필요해지게 된다.
+
+let cached: MongooseConnection = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn;
+
+  if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
+
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URL, {
+      dbName: "image-genius",
+      bufferCommands: false,
+    });
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+};

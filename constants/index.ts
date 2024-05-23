16a0998f5d7faac06e82a4db8e805d33f83439a9
scs0209 <tjdckdtn2463@naver.com>
2024-05-23 19:04:00 +0900

    feat: 이미지 변환 유형별 처리 기능 개선
    - 다양한 변환 유형을 동적으로 처리하도록 `AddTransformationTypePage`
      컴포넌트 업데이트.
    - 사용자 인증 확인 및 인증되지 않은 사용자는 로그인 페이지로 리디렉션 구현.
    - 사용자 데이터를 가져와 `TransformationForm`에 관련 정보를 전달.
    - 각 변환 유형에 대한 이해를 돕기 위해 `constants/index.ts`에 있는 `transformationTypes`에 상세한 주석 추가.
    - 패키지 의존성 업데이트: `@hookform/resolvers`, `@radix-ui/react-label`, `react-hook-form`, `zod` 추가.
    - 변환 유형 및 폼에 대한 새로운 TypeScript 타입을 `types/index.d.ts`에 추가.

--- constants/index.ts
+++ constants/index.ts
@@ -40,3 +40,63 @@ export const navLinks = [
     icon: "/assets/icons/bag.svg",
   },
 ];
+
+// 다양한 이미지 변환 작업데 대한 설정을 저장
+// 각 변환 작업은 고유의 키와 함께 여러 가지 속성을 가짐
+export const transformationTypes = {
+  // 이미지 복원을 위한 설정
+  restore: {
+    type: "restore",
+    title: "Restore Image",
+    subTitle: "Refine images by removing noise and imperfections",
+    config: { restore: true },
+    icon: "image.svg",
+  },
+  // 배경 제거를 위한 설정
+  removeBackground: {
+    type: "removeBackground",
+    title: "Background Remove",
+    subTitle: "Removes the background of the image using AI",
+    config: { removeBackground: true },
+    icon: "camera.svg",
+  },
+  // 이미지 채우기를 위한 설정
+  fill: {
+    type: "fill",
+    title: "Generative Fill",
+    subTitle: "Enhance an image's dimensions using AI outpainting",
+    config: { fillBackground: true },
+    icon: "stars.svg",
+  },
+  // 객체 제거를 위한 설정
+  remove: {
+    type: "remove",
+    title: "Object Remove",
+    subTitle: "Identify and eliminate objects from images",
+    // 제거 프롬프트, 그림자 제거 여부, 다중 제거 여부 포함
+    config: {
+      remove: { prompt: "", removeShadow: true, multiple: true },
+    },
+    icon: "scan.svg",
+  },
+  // 객체 재색을 위한 설정
+  recolor: {
+    type: "recolor",
+    title: "Object Recolor",
+    subTitle: "Identify and recolor objects from the image",
+    // 재색 프롬프트, 색상 변경 대상, 다중 재색 여부 포함
+    config: {
+      recolor: { prompt: "", to: "", multiple: true },
+    },
+    icon: "filter.svg",
+  },
+};
+
+// react-hook-form에 사용할 defaultValues(초깃값이 설정이 안되어있을 때 사용)
+export const defaultValues = {
+  title: "",
+  aspectRatio: "",
+  color: "",
+  prompt: "",
+  publicId: "",
+};
--- types/index.d.ts
+++ types/index.d.ts
@@ -14,3 +14,42 @@ declare type UpdateUserParams = {
   username: string;
   photo: string;
 };
+
+// ====== IMAGE PARAMS
+declare type TransformationTypeKey =
+  | "restore"
+  | "fill"
+  | "remove"
+  | "recolor"
+  | "removeBackground";
+
+declare type Transformations = {
+  restore?: boolean;
+  fillBackground?: boolean;
+  remove?: {
+    prompt: string;
+    removeShadow?: boolean;
+    multiple?: boolean;
+  };
+  recolor?: {
+    prompt?: string;
+    to: string;
+    multiple?: boolean;
+  };
+  removeBackground?: boolean;
+};
+
+// ====== URL QUERY PARAMS
+declare type SearchParamProps = {
+  params: { id: string; type: TransformationTypeKey };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+declare type TransformationFormProps = {
+  action: "Add" | "Update";
+  userId: string;
+  type: TransformationTypeKey;
+  creditBalance: number;
+  data?: IImage | null;
+  config?: Transformations | null;
+};

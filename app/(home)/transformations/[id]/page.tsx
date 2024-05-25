1209ca16c4101827893b1300d6a10f26144873fd
scs0209 <tjdckdtn2463@naver.com>
2024-05-25 16:30:47 +0900

    feat: TransformationPage에 이미지 삭제 기능 추가
    - DeleteConfirmation 컴포넌트를 TransformationPage에 import
    - 이미지 작가 정보를 가져오는 populateUser 함수에서 clerkId를 포함하도록
      수정

--- app/(home)/transformations/[id]/page.tsx
+++ app/(home)/transformations/[id]/page.tsx
@@ -7,7 +7,7 @@ import TransformImage from "@/components/shared/TransformImage";
 import { Button } from "@/components/ui/button";
 import { getImageById } from "@/lib/actions/image.actions";
 import { getImageSize } from "@/lib/utils";
-// import { DeleteConfirmation } from "@/components/shared/DeleteConfirmation";
+import DeleteConfirmation from "@/components/shared/DeleteConfirmation";
 
 const TransformationPage = async ({ params: { id } }: SearchParamProps) => {
   const { userId } = auth();
@@ -91,7 +91,7 @@ const TransformationPage = async ({ params: { id } }: SearchParamProps) => {
               </Link>
             </Button>
 
-            {/* <DeleteConfirmation imageId={image._id} /> */}
+            <DeleteConfirmation imageId={image._id} />
           </div>
         )}
       </section>

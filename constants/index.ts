58aaf0040e1473223dc09bd413f037ce753c1b61
scs0209 <tjdckdtn2463@naver.com>
2024-05-25 22:57:44 +0900

    feat: 크레딧 구매 페이지 생성
    - 크레딧 구매 페이지에 다양한 플랜 및 가격 옵션 구현
    - 사용자가 로그인되어 있는지 확인하는 인증 통합
    - 사용자 정보를 가져오는 `getUserById` 함수 추가
    - 크레딧 아이템 및 버튼에 `sky` 색상 팔레트를 사용하도록 `globals.css`
      업데이트
    - 다양한 크레딧 패키지 옵션을 포함한 `plans` 상수 생성
    - 거래 관련 타입 정의를 `types/index.d.ts`에 추가

--- constants/index.ts
+++ constants/index.ts
@@ -41,6 +41,84 @@ export const navLinks = [
   },
 ];
 
+export const plans = [
+  {
+    _id: 1,
+    name: "Free",
+    icon: "/assets/icons/free-plan.svg",
+    price: 0,
+    credits: 20,
+    inclusions: [
+      {
+        label: "20 Free Credits",
+        isIncluded: true,
+      },
+      {
+        label: "Basic Access to Services",
+        isIncluded: true,
+      },
+      {
+        label: "Priority Customer Support",
+        isIncluded: false,
+      },
+      {
+        label: "Priority Updates",
+        isIncluded: false,
+      },
+    ],
+  },
+  {
+    _id: 2,
+    name: "Pro Package",
+    icon: "/assets/icons/free-plan.svg",
+    price: 40,
+    credits: 120,
+    inclusions: [
+      {
+        label: "120 Credits",
+        isIncluded: true,
+      },
+      {
+        label: "Full Access to Services",
+        isIncluded: true,
+      },
+      {
+        label: "Priority Customer Support",
+        isIncluded: true,
+      },
+      {
+        label: "Priority Updates",
+        isIncluded: false,
+      },
+    ],
+  },
+  {
+    _id: 3,
+    name: "Premium Package",
+    icon: "/assets/icons/free-plan.svg",
+    price: 199,
+    credits: 2000,
+    inclusions: [
+      {
+        label: "2000 Credits",
+        isIncluded: true,
+      },
+      {
+        label: "Full Access to Services",
+        isIncluded: true,
+      },
+      {
+        label: "Priority Customer Support",
+        isIncluded: true,
+      },
+      {
+        label: "Priority Updates",
+        isIncluded: true,
+      },
+    ],
+  },
+];
+
 // 다양한 이미지 변환 작업데 대한 설정을 저장
 // 각 변환 작업은 고유의 키와 함께 여러 가지 속성을 가짐
 export const transformationTypes = {

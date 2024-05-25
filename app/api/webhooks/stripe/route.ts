77356801aa0f72d49aebf2f95fe6ecb5dba25ffe
scs0209 <tjdckdtn2463@naver.com>
2024-05-25 23:49:06 +0900

    fix: credit 구매 제대로 반영 안되는 에러 수정

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -27,7 +27,7 @@ export async function POST(request: Request) {
       stripeId: id,
       amount: amount_total ? amount_total / 100 : 0,
       plan: metadata?.plan || "",
-      credits: Number(metadata?.credits) || 0,
+      credits: Number(metadata?.credit) || 0,
       buyerId: metadata?.buyerId || "",
       createdAt: new Date(),
     };
